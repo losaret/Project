4b18091a46437f69834c087b6f2ddcdd87a47d88
Grigrory <losaret879@yandex.ru>
2019-09-11 17:46:08 +0300

    Set PSQL

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project',
+        'USER': 'grigory',
+        'PASSWORD': 'blogpass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
