1b946a020f33bfca505c34a26948ea944b78776d
Grigrory <losaret879@yandex.ru>
2019-09-11 17:08:10 +0300

    First run

---
+++ blog/blog_site/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+
+
+class Index(View):
+    def get(self,request):
+        params = {}
+        params["name"] = "Django"
+        return render(request, 'base.html', params)
+    def post(self,request):
+        return HttpResponse('Request post')
